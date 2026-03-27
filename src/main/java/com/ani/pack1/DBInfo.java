9c3ae234c602a514c8ac116665fe44800c1ba7fa
Sanghamitraswain2002 <swainsanghamitra617@gmail.com>
2026-03-27 23:37:27 +0530

    Update DBInfo.java

--- src/main/java/com/ani/pack1/DBInfo.java
+++ src/main/java/com/ani/pack1/DBInfo.java
@@ -3,6 +3,6 @@ package com.ani.pack1;
 public interface DBInfo 
 {
 	String dbUrl ="jdbc:oracle:thin:@localhost:1521:orcl";
-	String dbName="system";
-	String dbPwd = "manager"; 
+	String dbName="your_UserName";
+	String dbPwd = "Your_Password"; 
 }
